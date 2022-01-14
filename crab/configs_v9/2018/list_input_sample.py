9c536f4c9afaa6f1ba1e5c953f04565f86c03e9f
Efe <efe.yazgan@cern.ch>
2022-01-14 17:33:40 +0100

    updates cfgs and cfg creator for 2018

--- crab/configs_v9/2018/list_input_sample.py
+++ crab/configs_v9/2018/list_input_sample.py
@@ -4,28 +4,44 @@ import os
 import sys
 import re
 
-import os
 for filename in os.listdir("../2017"):
-#for filename in os.listdir(os.getcwd():
-#   with open(os.path.join(os.getcwd(), filename), 'r') as f:
+   print("-------------------------------------------------------------------------------------")
    if "cfg" in filename:
        sample = os.popen(f'grep config.Data.inputDataset ../2017/{filename}').read().split("=")
        if len(sample) > 1:
-          print(filename)
-          sample = sample[1].replace(" ","").replace("'","")
-          head, sep, tail = sample.partition('UL')
+          era = filename.split("_cfg")[0][-1]
+          sample_and_era = filename.split("_cfg")[0].replace(filename.split("_cfg")[0][-1],"_"+filename.split("_cfg")[0][-1])
+          print(sample_and_era)
+          sample_tmp = sample[1].replace(" ","").replace("'","")
+          head, sep, tail = sample_tmp.partition('UL')
           head = head+sep
           head = head.replace("2017","2018")
           if "2018F" in head:
               continue
-          if "SIM" in sample:
+          if "SIM" in sample_tmp:
               head = head.replace("UL","UL18*/NANOAODSIM\"")
           else:
               head = head.replace("DoubleEG","EGamma")
               head = head.replace("UL","UL2018*/NANOAOD\"").replace("2018E","2018A")
           dasstr = f'dasgoclient -query=\"dataset={head} | grep NanoAODv9 | grep -v JMENano'
-#          print(head)
-#          print(sample)
           print(dasstr)
-          print(os.popen(dasstr).read())
-          print("-------------------------------------------------------------------------------------")
+          sample[1] = os.popen(dasstr).read().rstrip()
+          sampleline = os.popen(dasstr).read().rstrip().splitlines()
+          print(sampleline)       
+          filename2018 = filename
+          for x in range(len(sampleline)):
+              new_line = sample[0]+"= '"+sampleline[x]+"'\n" 
+              if x > 0:
+                  filename2018 = filename.split("_cfg.py")[0] + "_ext" + str(x) + "_cfg.py" 
+                  print(filename2018)
+              with open(f"../2017/{filename}", 'r') as f:
+                  with open(os.path.join(os.getcwd(), filename2018), 'w') as fo: 
+                      for line in f:
+                          line = line.replace("DoubleEG","EGamma")
+                          line = line.replace("Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt","Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt")
+                          if "config.Data.inputDataset" not in line.strip("\n"):
+                              fo.write(line)
+                          else:
+                              fo.write(new_line)
+                  fo.close()
+              f.close()
--- crab/configs_v9/2018/www_ext1_cfg.py
+++ crab/configs_v9/2018/www_ext1_cfg.py
@@ -3,26 +3,27 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'MET_E'
+config.General.requestName = 'WWW_1'
 config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
-config.JobType.scriptExe = 'crab_script_dataE.sh'
+config.JobType.scriptExe = 'crab_script.sh'
 # hadd nano will not be needed once nano tools are in cmssw
-config.JobType.inputFiles = ['crab_script.py', '../scripts/haddnano.py','keep_and_drop.txt','Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt']
+config.JobType.inputFiles = ['crab_script.py', '../scripts/haddnano.py','keep_and_drop.txt']
 config.JobType.sendPythonFolder = True
 
 config.section_("Data")
-#config.Data.inputDataset = '/MET/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD'
+config.Data.inputDataset = '/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1_ext1-v2/NANOAODSIM'
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 80
-config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
+config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
 config.Data.publication = False
-config.Data.outputDatasetTag = 'MET_E'
+config.Data.outputDatasetTag = 'WWW_1'
 
 config.section_("Site")
 config.Site.storageSite = "T3_CH_CERNBOX"
