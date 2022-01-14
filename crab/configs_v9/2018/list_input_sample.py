4c2935d3b9214d0ec13b75ae1db760ba31fc1bc8
Efe <efe.yazgan@cern.ch>
2022-01-14 10:48:51 +0100

    update

--- crab/configs_v9/2018/list_input_sample.py
+++ crab/configs_v9/2018/list_input_sample.py
@@ -5,15 +5,11 @@ import sys
 import re
 
 import os
-#
-print("Read the files from 2017 configurations and convert them to search for 2018 datasets")
-#
 for filename in os.listdir(os.getcwd()):
 #   with open(os.path.join(os.getcwd(), filename), 'r') as f:
    if "cfg" in filename:
-       sample = os.popen(f'grep config.Data.inputDataset ../2017/{filename}').read().split("=")
+       sample = os.popen(f'grep config.Data.inputDataset {filename}').read().split("=")
        if len(sample) > 1:
-          print(filename)
           sample = sample[1].replace(" ","").replace("'","")
           head, sep, tail = sample.partition('UL')
           head = head+sep
@@ -25,7 +21,7 @@ for filename in os.listdir(os.getcwd()):
           else:
               head = head.replace("DoubleEG","EGamma")
               head = head.replace("UL","UL2018*/NANOAOD\"").replace("2018E","2018A")
-          dasstr = f'dasgoclient -query=\"dataset={head} | grep NanoAODv9 | grep -v JMENano'
+          dasstr = f'dasgoclient -query=\"dataset={head} | grep NanoAODv9'
 #          print(head)
 #          print(sample)
           print(dasstr)
