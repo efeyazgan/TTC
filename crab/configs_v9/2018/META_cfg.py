7c2de916e1c3da38221cc598934c087cedb7d334
Efe <efe.yazgan@cern.ch>
2022-01-14 18:15:46 +0100

    almost final version of crab cfgs for 2018

--- crab/configs_v9/2018/META_cfg.py
+++ crab/configs_v9/2018/META_cfg.py
@@ -3,27 +3,26 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'TTTo1L'
+config.General.requestName = 'MET_E'
 config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
-config.JobType.scriptExe = 'crab_script_2018.sh'
+config.JobType.scriptExe = 'crab_script_2018_dataE.sh'
 # hadd nano will not be needed once nano tools are in cmssw
-config.JobType.inputFiles = ['crab_script_2018.py', '../scripts/haddnano.py','keep_and_drop.txt']
+config.JobType.inputFiles = ['crab_script_2018.py', '../scripts/haddnano.py','keep_and_drop.txt','Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt']
 config.JobType.sendPythonFolder = True
 
 config.section_("Data")
-config.Data.inputDataset = '/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM'
+#config.Data.inputDataset = '/MET/Run2018A-UL2018_MiniAODv2_NanoAODv9-v2/NANOAOD'
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-#config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 1
-config.Data.totalUnits = -1
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 80
+config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
 config.Data.publication = False
-config.Data.outputDatasetTag = 'TTTo1L'
+config.Data.outputDatasetTag = 'MET_E'
 
 config.section_("Site")
 config.Site.storageSite = "T3_CH_CERNBOX"
